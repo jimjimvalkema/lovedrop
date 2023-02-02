f70a42e3eef930309c6c0cb1dc8b50333b54abbc
jimjimvalkema <jjvalkema@protonmail.com>
2023-02-02 17:54:20 +0100

    small code clean up

--- contracts/MiladyDrop.sol
+++ contracts/MiladyDrop.sol
@@ -5,28 +5,33 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract MiladyDrop {
-    IERC20 public airdropToken; //public?
-    IERC721 public requiredNFT;
+    IERC20 private airdropToken;
+    IERC721 private requiredNFT;
+    address public requiredNFTAddress;
+    address public airdropTokenAddress;
+
     uint256 airdropAmount;
-
-    constructor(address requiredNFTAddress, address airdropTokenAddress, uint256 amount) {
-        requiredNFT = IERC721(requiredNFTAddress);
-        airdropToken = IERC20(airdropTokenAddress);
-        airdropAmount = amount;
-        airdropToken.approve(address(this), 1000000000000);
-        }
-
     mapping(uint256 => bool) public claimedIds; //TODO tight packing and getter
 
-    function claim(uint256[] memory ids) public {
+
+    constructor(address _requiredNFTAddress, address _airdropTokenAddress, uint256 _amount) {
+        requiredNFT = IERC721(_requiredNFTAddress);
+        airdropToken = IERC20(_airdropTokenAddress);
+        airdropAmount = _amount;
+
+        //store addresses for easier acces
+        requiredNFTAddress = _requiredNFTAddress;
+        airdropTokenAddress = _airdropTokenAddress;
+        }
+
+    function claim(uint256[] memory _ids) public {
         uint256 currentId;
-        for (uint i=0; i < ids.length; i++) {
-            currentId = ids[i];
+        for (uint i=0; i < _ids.length; i++) {
+            currentId = _ids[i];
             require(requiredNFT.ownerOf(currentId) == msg.sender, "you dont own one or more of these nfts"); //msg.esender bad?
-            require(claimedIds[currentId] == false, "oops already claimed");
+            require(claimedIds[currentId] == false, "one or more of these ids is already claimed");
             claimedIds[currentId] = true;
             airdropToken.transfer(address(msg.sender), airdropAmount);
         }
     }
-
 }
