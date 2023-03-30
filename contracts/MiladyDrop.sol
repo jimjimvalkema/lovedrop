09f7d8533d94fb3231d5d96c92761664b24003be
jjvalkema <jjvalkema@protonmail.com>
2023-03-30 14:04:58 +0200

    made a function to claim multiple drops 1 address owns

--- contracts/MiladyDrop.sol
+++ contracts/MiladyDrop.sol
@@ -23,7 +23,13 @@ contract MiladyDrop is IMiladyDrop {
 
     // This is a packed array of booleans.
     mapping(uint256 => uint256) private claimedBitMap;
-    
+
+    struct ClaimData {
+        uint256 index;
+        uint256 id;
+        uint256 amount;
+        bytes32[] merkleProof;
+    }    
 
 
     constructor(address _requiredNFTAddress, address _airdropTokenAddress, uint256 _amount, bytes32 _merkleRoot) {
@@ -61,17 +67,21 @@ contract MiladyDrop is IMiladyDrop {
         }
     }
 
-    function claim(uint256 index, uint256 id, uint256 amount, bytes32[] calldata merkleProof)
-        public
-        virtual
-        override
-    {
+    function verifyClaim(uint256 index, uint256 id, uint256 amount, bytes32[] calldata merkleProof) private view {
         if (isClaimed(index)) revert AlreadyClaimed();
         require(IERC721(requiredNFTAddress).ownerOf(id) == msg.sender, "you dont own one or more of these nfts"); //msg.esender bad?
 
         // Verify the merkle proof.
         bytes32 node = keccak256(abi.encodePacked(index, id, amount));
         if (!MerkleProof.verify(merkleProof, merkleRoot, node)) revert InvalidProof();
+    }
+
+    function claim(uint256 index, uint256 id, uint256 amount, bytes32[] calldata merkleProof)
+        public
+        virtual
+        override
+    {
+        verifyClaim(index, id, amount, merkleProof);
 
         // Mark it claimed and send the token.
         _setClaimed(index);
@@ -79,5 +89,23 @@ contract MiladyDrop is IMiladyDrop {
         IERC20(airdropTokenAddress).transfer(address(msg.sender), amount);
 
         emit Claimed(index, id, amount);
+    }
+
+    function claimMultiple(ClaimData[] calldata _claims) public virtual {
+        uint256 totalAmount = 0;
+        //TODO keep claiming even if 1 fails (idk might be bad for gas though)
+        for (uint i=0; i < _claims.length; i++) {
+            // TODO check if this is better for gas or just read struct directly  
+            uint256 index = _claims[i].index;
+            uint256 amount = _claims[i].amount; 
+            verifyClaim(index, _claims[i].id, amount,  _claims[i].merkleProof);
+            
+            // Mark it claimed add amount
+            _setClaimed(index);
+            totalAmount += amount;
+        }
+        //Send the total amount to user
+        IERC20(airdropTokenAddress).transfer(address(msg.sender), totalAmount);
+
     }
 }
