59773d576e616d9abc507485f384ae70c7057b4a
jjvalkema <jjvalkema@protonmail.com>
2023-03-31 13:02:35 +0200

    made a claim page work with claimMultiple to claim all

--- ui/claim/scripts/app.js
+++ ui/claim/scripts/app.js
@@ -14,8 +14,6 @@ let signer = null;
 let currentFileString = null;
 let currenFileName = null;
 
-let balanceMapJson = null;
-
 //abis global variable
 let mildayDropAbi = null;
 let ERC20ABI = null;
@@ -38,8 +36,7 @@ async function connectSigner() {
 }
 
 async function getMiladyDropContract(provider, urlVars) {
-    console.log(urlVars["mildayDropAddress"])
-    mildayDropAddress = ethers.utils.getAddress(urlVars["mildayDropAddress"])
+    mildayDropAddress = urlVars["mildayDropAddress"]
     //const mildayDropAbi = 
     // The Contract object
     mildayDropContract = new ethers.Contract(mildayDropAddress, mildayDropAbi, provider);
@@ -65,22 +62,16 @@ async function getNftContract() {
 
 async function getUserNftIds(userAddress) {
     var ids = [];
-    let userBalance = parseInt(await nftContract.balanceOf(userAddress), 16);
+    let userBalance = (await nftContract.balanceOf(userAddress)).toNumber();
     for (let i = 1; i < userBalance; i++) {
-        ids.push(parseInt(await nftContract.tokenOfOwnerByIndex(userAddress, i), 16));
+        ids.push((await nftContract.tokenOfOwnerByIndex(userAddress, i)).toNumber());
 
     }
     return ids;
 }
 
-function getIndex(id) {
-    //TODO get merkle hash from contract then ipfs etc
-    return balanceMapJson["claims"][id]["index"];
-}
-
-function isClaimed(id) {
-    index = getIndex(id)
-    return window.mildayDropWithSigner.isClaimed(index);
+function isClaimed(claimData) {
+    return mildayDropWithSigner.isClaimed(claimData["index"]);
 }
 
 async function getUrlVars() {
@@ -104,49 +95,57 @@ function isWalletConnected() {
     }
 }
 
-
 async function loadAllContracts() {
     urlVars = await getUrlVars();
-    console.log(ethers.utils.getAddress(urlVars['mildayDropAddress']));
     mildayDropContract = await getMiladyDropContract(provider, urlVars);
-    window.mildayDropWithSigner = mildayDropContract.connect(signer);
+    mildayDropWithSigner = mildayDropContract.connect(signer);
     nftContract = await getNftContract();
     airdropTokenContract = await getAirdropTokenContract();
-    return [mildayDropContract, window.mildayDropWithSigner, nftContract, airdropTokenContract];
+    return [mildayDropContract, mildayDropWithSigner, nftContract, airdropTokenContract];
 }
 
-function getProof(id) {
-    const index = balanceMapJson["claims"][id]["index"];
-    const amount = parseInt(balanceMapJson["claims"][id]["amount"], 16)
-    const proof = balanceMapJson["claims"][id]["proof"];
+function getProof(id, claimData) {
+    const index = claimData["index"];
+    const amount = parseInt(claimData["amount"], 16)
+    const proof = claimData["proof"];
     return [index, id, amount, proof]
+
 }
+
 
 
 //TODO update with merkle stuff :D
-async function claimAll() {
+async function claimAll(ipfsIndex=window.ipfsIndex) {
     if (isWalletConnected()) {
         //await getAllContracts();
-        let userAddress = signer.getAddress();
+        let userAddress = await signer.getAddress();
         var user_nfts = await getUserNftIds(userAddress, nftContract);
         var unclaimed_proofs = [];
         for (let i = 0; i < user_nfts.length; i++) {
             let id = user_nfts[i]
-            if (! await isClaimed(id)) {
-                unclaimed_proofs.push(getProof(id));
+            let claimData = await ipfsIndex.getIdFromIndex(id) //TODO handle error if doesn't exist and message to user
+            if (! await isClaimed(claimData)) {
+                unclaimed_proofs.push(getProof(id, claimData));
             }
         }
-        window.mildayDropWithSigner.multiClaim(unclaimed_proofs);
+        if (unclaimed_proofs.length == 0) {
+            let nftBalance =  (await nftContract.balanceOf(userAddress)).toNumber();
+            let message = `found ${nftBalance} nfts but none have tokens to be claimed`;
+            console.log(message);
+            document.getElementById("message").innerHTML = message;
+            return 0
+        }
+        mildayDropWithSigner.claimMultiple(unclaimed_proofs); //TODO unclaimed_proofs to the struct
     }
     return 0
 };
 
-async function claim(id) {
+async function claim(id, ipfsIndex=window.ipfsIndex) {
+    const claimData = await ipfsIndex.getIdFromIndex(id)
     if (isWalletConnected()) {
-        if (! await isClaimed(id)) {
-            let proof = await getProof(id);
-            console.log(proof);
-            window.mildayDropWithSigner.claim(...proof);
+        if (! await isClaimed(claimData)) {
+            let proof = await getProof(id, claimData);
+            mildayDropWithSigner.claim(...proof);
         }
     }
     return 0;
@@ -235,7 +234,7 @@ function csvToBalanceMapJSON() {
     let merkle = uniswapMerkle.parseBalanceMapOnIds(formatedBalanceMap);
     let newFilename = currenFileName.split(".")[0] + ".json";
     downloadString(JSON.stringify(merkle, null, 2), "json", newFilename)
-    balanceMapJson = merkle; //TODO get merkle hash from contract then ipfs etc
+    let balanceMapJson = merkle; //TODO get merkle hash from contract then ipfs etc
 }
 
 function goToclaim() {
@@ -251,67 +250,40 @@ async function test() {
         return 0
     }
 
-    proofsFile = await fetch('./timesTwo.json');
-    balanceMapJson = await proofsFile.json();
-
-    // The MetaMask plugin also allows signing transactions to
-    // send ether and pay to change state within the blockchain.
-    // For this, you need the account signer...formatBalanceMapIds(balanceMap["data"]) 
-    //await getAllContracts();p
-
-    let userAddress = await signer.getAddress();
-
-    console.log(userAddress);
-    console.log(await isClaimed(1));
-    console.log(await getUserNftIds(userAddress));
-    console.log(parseInt(await airdropTokenContract.balanceOf(userAddress), 16))
-    console.log(getProof(1));
-    // a = splitObject(tempMerkle["claims"], 780);
-    // console.log("we got files :D:  " + a.length)
-    // //downloadString(JSON.stringify(a["0"], null, 2), "json", "hi")
-
-    // var zip = new JSZip();
-    // for(let i=0; i<a.length; i++) {
-    //     let filename = "0-"+ (i*780).toString()+".json"
-    //     zip.file(filename+".json", JSON.stringify(a[i], null, 2));
-    // }
-    // //var img = zip.folder("images");
-    // //img.file("smile.gif", imgData, {base64: true});
-    // zip.generateAsync({type:"blob"})
-    // .then(function(content) {
-    //     // see FileSaver.js
-    //     saveAs(content, "example.zip");
-    // });
-
-    //const ipfsClient = require('ipfs-http-client');
-    const ipfsApi = urlVars["ipfsApi"]   
-    let auth = null; 
-    if(urlVars["projectId"]!=null) {
-        const projectId = urlVars["projectId"]
-        const projectSecret = urlVars["projectSecret"]
-        auth = "Basic " + btoa(projectId+ ":" + projectSecret);
-
-    } else {
-        auth = null
-    }
-
-    window.ipfsIndex = new ipfsIndexer(ipfsApi, auth, isGateway=false);
-
-    window.newHashWithIndex =  await window.ipfsIndex.createIpfsIndex(balanceMapJson, splitSize=50);//"bafybeigdvozivwvzn3mrckxeptstjxzvtpgmzqsxbau5qecovlh4r57tci"
-    console.log(window.newHashWithIndex)
-
-    await window.ipfsIndex.loadIndex(window.newHashWithIndex);
-    console.log(window.ipfsIndex.index);
+    window.newHashWithIndex = "bafybeigdvozivwvzn3mrckxeptstjxzvtpgmzqsxbau5qecovlh4r57tci"//await window.ipfsIndex.createIpfsIndex(balanceMapJson, splitSize=780);//"bafybeigdvozivwvzn3mrckxeptstjxzvtpgmzqsxbau5qecovlh4r57tci"
+    console.log(`index obj: ${JSON.stringify(window.ipfsIndex.index, null, 2)}`);
     console.log(window.ipfsIndex.dropsRootHash);
+    console.log(await window.ipfsIndex.getIdFromIndex(20));
+    claim(8, window.ipfsIndex);
 };
 
 window.onload = runOnLoad;
 
 async function runOnLoad() {
-    let mildayDropAbiFile = await fetch('./abi/mildayDropAbi.json');
-    let ERC721ABIFile = await fetch('./abi/ERC721ABI.json');
-    let ERC20ABIFile = await fetch('./abi/ERC20ABI.json');
+    window.urlVars = await getUrlVars();
+    let mildayDropAbiFile = await fetch('./../abi/mildayDropAbi.json');
+    let ERC721ABIFile = await fetch('./../abi/ERC721ABI.json');
+    let ERC20ABIFile = await fetch('./../abi/ERC20ABI.json');
     mildayDropAbi = await mildayDropAbiFile.json();
     ERC721ABI = await ERC721ABIFile.json();
     ERC20ABI = await ERC20ABIFile.json();
+
+    //TODO make function to connect ipfs indexer and option for is gateway in ui
+    //TODO test if can connect 
+    //connect api
+    window.ipfsApi= window.urlVars["ipfsApi"]   
+    window.auth = null; 
+    if(window.urlVars["projectId"]!=null) {
+        const projectId = window.urlVars["projectId"]
+        const projectSecret = window.urlVars["projectSecret"]
+        window.auth  = "Basic " + btoa(projectId+ ":" + projectSecret);
+    
+    } else {
+        window.auth  = null
+    }
+
+    //load indexer
+    window.newHashWithIndex = "bafybeigdvozivwvzn3mrckxeptstjxzvtpgmzqsxbau5qecovlh4r57tci"//await window.ipfsIndex.createIpfsIndex(balanceMapJson, splitSize=780);//"bafybeigdvozivwvzn3mrckxeptstjxzvtpgmzqsxbau5qecovlh4r57tci"
+    window.ipfsIndex = new ipfsIndexer(window.ipfsApi, window.auth , isGateway=false);
+    await window.ipfsIndex.loadIndex(window.newHashWithIndex);
 }
