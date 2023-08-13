99037ebb89a517ea5d066d7930443e5e635ab96e
jjvalkema <jjvalkema@protonmail.com>
2023-08-13 19:00:35 +0200

    Made the uricache syncable from a link

--- ui/scripts/uriHandler.js
+++ ui/scripts/uriHandler.js
@@ -2,15 +2,19 @@ const delay = ms => new Promise(res => setTimeout(res, ms));
 
 class uriHandler {
     contractObj = null;
-    uriType = "standard"
+    extraUriMetaData = null;
     uriCache = []
     baseURICache = null;
-    constructor(contractObj,typesJsonFile ,uriType=null) {
+    ipfsGateway = null;
+    //TODO fix naming
+    constructor(contractObj, extraUriMetaDataFile="./claim/scripts/extraUriMetaDataFile.json", _ipfsGateway="http://localhost:48084/", _extraUriMetaData = undefined) {
         this.contractObj = contractObj;
-        if (uriType==null) {
-            this.uriType = this.getURIType(contractObj, typesJsonFile)
+        this.ipfsGateway = _ipfsGateway
+        if (extraUriMetaDataFile) {
+            this.extraUriMetaData = this.getExtraUriMetaData(contractObj, extraUriMetaDataFile)
+        } else {
+            this.extraUriMetaData = _extraUriMetaData
         }
-
     }
 
     async getImage(id) {
@@ -20,10 +24,10 @@ class uriHandler {
                 'Content-Type': '*'
             }
         }
-        console.log(this.contractObj);
-        console.log(this.uriType);
-        let imgURL = ""; 
-        switch (this.uriType) {
+        //console.log(this.contractObj);
+        //console.log(this.uriType);
+        let imgURL = "";
+        switch ((await this.extraUriMetaData).type) {
             case "standard":
                 imgURL = (await fetch(await this.contractObj.tokenURI(id), reqObj))["image"];
                 break;
@@ -32,14 +36,14 @@ class uriHandler {
                 imgURL = `https://www.miladymaker.net/milady/${id}.png`;
                 break;
             default:
-                imgURL = `https://www.miladymaker.net/milady/${id}.png`;
-                //imgURL = (await fetch(await this.contractObj.tokenURI(id), reqObj))["image"];
+                //imgURL = `https://www.miladymaker.net/milady/${id}.png`;
+                imgURL = (await fetch(await this.contractObj.tokenURI(id), reqObj))["image"];
                 break;
         }
         console.log(imgURL)
         return imgURL;
-            
-  
+
+
     }
 
     async getBaseURI() {
@@ -51,36 +55,34 @@ class uriHandler {
         return this.baseURICache;
     }
 
-    async getURIType(contractObj, typesJsonFile) {
-        let typesJson = await (await fetch(typesJsonFile)).json();
+    async getExtraUriMetaData(contractObj, extraUriMetaDataFile) {
+        let extraUriMetaData = await (await fetch(extraUriMetaDataFile)).json();
         const contractAddr = (await contractObj.address).toLowerCase()
-        console.log("bbbbbbbb")
-        console.log(typesJson)
-        console.log(contractAddr)
-        if (contractAddr in typesJson) {
-            return typesJson[contractAddr]["type"]
+        if (contractAddr in extraUriMetaData) {
+            return extraUriMetaData[contractAddr]
         } else {
-            return "standard"
+            console.log(`Nft contract: ${contractAddr} not found in extraUriMetaDataFile: ${extraUriMetaDataFile}, setting to default values`)
+            return { "type": "standart", "name": "standart", "ScrapedUriData": undefined }
         }
-        
-    } 
 
-    async getUrisInBulk(startId=0,  endId=null, idList =null) {
+    }
+
+    async getUrisInBulk(startId = 0, endId = null, idList = null) {
         let uris = [];
         //iter from startId to endId
         if (idList === null) {
             //do entire supply if endId is null
-            if (endId===null) {
-                endId=(await this.contractObj.totalSupply()).toNumber()
+            if (endId === null) {
+                endId = (await this.contractObj.totalSupply()).toNumber()
             }
-            for (let i=startId; i<endId; i++) {
+            for (let i = startId; i < endId; i++) {
                 let id = i
-                uris.push( this.getTokenUriNoCache(id));
+                uris.push(this.getTokenUriNoCache(id));
             }
-        
-        //iter ofer idList
+
+            //iter ofer idList
         } else {
-            for (let i=0; i<idList.length; i++) {
+            for (let i = 0; i < idList.length; i++) {
                 let id = idList[i]
                 uris.push(this.getTokenUriNoCache(id));
             }
@@ -89,40 +91,80 @@ class uriHandler {
     }
 
 
-    async syncUriCacheBulkInChunks(startId=0,  endId=null, idList =null, chunkSize=100) {
+    async syncUriCacheByScraping(startId = 0, endId = null, chunkSize = 100, idList=null) {
+        let allUris = [];
         //iter from startId to endId
         if (idList === null) {
             //do entire supply if endId is null
-            if (endId===null) {
-                endId=(await this.contractObj.totalSupply()).toNumber()
+            if (endId === null) {
+                endId = (await this.contractObj.totalSupply()).toNumber()
             }
-            
+
             let endChunk = 0;
-            for (let startChunk = 0; startChunk<endId;) {
+            for (let startChunk = 0; startChunk < endId;) {
                 endChunk += chunkSize;
                 console.log(startChunk, endChunk);
-                if (endChunk>endId) { //brain cooked prob not good fix TODO
-                    endChunk=endId
+                if (endChunk > endId) { //brain cooked prob not good fix TODO
+                    endChunk = endId
                 }
-                let uris = await this.getUrisInBulk(startChunk,endChunk);
+                let uris = await this.getUrisInBulk(startChunk, endChunk);
                 //console.log("sdf")
                 //console.log(uris)
-                this.uriCache = [...this.uriCache, ...uris];
+                allUris = [...allUris, ...uris];
 
                 startChunk += chunkSize
             }
-        
-        //iter ofer idList
-        } else {console.error("TODO not implemeted")}
+
+            //iter ofer idList
+        } else { console.error("TODO not implemeted") }
         // else {
         //     for (let i=0; i<idList.length; i++) {
         //         let id = idList[i]
         //         this.uriCache[id] = this.getTokenUriNoCache(id);
         //     }
         // }
-        return this.uriCache
+        return allUris
     }
 
+    async getUrlByProtocol(urlString) {
+        let urlObj = new URL(urlString)
+        switch (urlObj.protocol) {
+            case ("ipfs:"):
+                return await fetch(`${this.ipfsGateway}ipfs${urlObj.pathname.slice(1)}`);
+            case ("http:"):
+                return await fetch(urlString);
+            case ("https"):
+                return await fetch(urlString);
+        }
+    }
+
+    async syncUriCache(startId=0, endId=null, chunkSize=100) {
+        console.log((await this.extraUriMetaData).scrapedUriData)
+        if ((await this.extraUriMetaData).scrapedUriData) {
+            this.uriCache = await (await this.getUrlByProtocol((await this.extraUriMetaData).scrapedUriData)).json()
+        } else {
+            console.log(`no premade metadata found for ntf contract: ${await this.contractObj.address} :( collecting attribute manually!`)
+            //syncUriCacheByScraping already
+            //this.uriCache = 
+            this.uriCache =  await this.syncUriCacheByScraping(startId, endId, chunkSize)
+        }
+    }
+
+    async getTokenUriNoCache(id) {
+        //const reqObj = {method: 'GET'}
+        let retries = 0;
+        while (retries < 10) {
+            try {
+                const URI = await (await fetch(`${await this.getBaseURI()}${id}`)).json();
+                return URI
+            } catch (error) {
+                console.log(`errored on id: ${id} tried ${retries} times`)
+                console.log(`error is: ${error}`);
+                await delay(500);
+            }
+            retries += 1;
+        }
+    }
 
     async getTokenUri(id) {
         if (this.uriCache[id]) {
@@ -134,45 +176,29 @@ class uriHandler {
 
     }
 
-    async getTokenUriNoCache(id) {
-        //const reqObj = {method: 'GET'}
-        let retries = 0;
-        while (retries < 10) {
-            try {
-                const URI =  await (await fetch(`${await this.getBaseURI()}${id}`)).json();
-                return URI
-            } catch(error) {
-                console.log(`errored on id: ${id} tried ${retries} times`)
-                console.log(`error is: ${error}`);
-                await delay(500);
-            }
-            retries += 1;
-        }
-    }
-
     async hasAttribute(id, attribute) {
         //attribute = {"trait_type": "Background","value": "bjork"},
         //just incase this standart changes
         let attributeFormat = {
-            pathToAttributeList : ["attributes"],
-            traitTypeKey : "trait_type",
-            valueKey : "value"
+            pathToAttributeList: ["attributes"],
+            traitTypeKey: "trait_type",
+            valueKey: "value"
         }
         let tokenURI = null
         try {
             tokenURI = await this.getTokenUri(id)
-        } catch(error) {
+        } catch (error) {
             console.error(`couldn't get token uri from token id: ${id} at base URI ${await this.getBaseURI()}. The error below is wat triggered this:`)
             console.error(error);
             return false
         }
         //console.log(tokenURI);
-        for (let i=0; i<attributeFormat.pathToAttributeList.length; i++) {
+        for (let i = 0; i < attributeFormat.pathToAttributeList.length; i++) {
             tokenURI = tokenURI[attributeFormat.pathToAttributeList[i]]
         }
         let attributeList = tokenURI;
-        
-        for (let i=0; i<attributeList.length; i++) {
+
+        for (let i = 0; i < attributeList.length; i++) {
             let attributeType = JSON.stringify(attribute[attributeFormat.traitTypeKey]);
             let nftAttributeType = JSON.stringify(attributeList[i][attributeFormat.traitTypeKey]);
             let attributeValue = JSON.stringify(attribute[attributeFormat.valueKey]);
@@ -188,13 +214,13 @@ class uriHandler {
         return false
     }
 
-    async getIdsByAttribute(attribute, startId=0,  endId=null, idList =null) {
+    async getIdsByAttribute(attribute, startId = 0, endId = null, idList = null) {
         let matchingIds = []
         if (idList === null) {
-            if (endId===null) {
-                endId=(await this.contractObj.totalSupply()).toNumber()
+            if (endId === null) {
+                endId = (await this.contractObj.totalSupply()).toNumber()
             }
-            for (let i=startId; i<endId; i++) {
+            for (let i = startId; i < endId; i++) {
                 let id = i
                 //console.log(id)
                 if (await this.hasAttribute(id, attribute) === true) {
@@ -202,7 +228,7 @@ class uriHandler {
                 }
             }
         } else {
-            for (let i=0; i<idList.length; i++) {
+            for (let i = 0; i < idList.length; i++) {
                 let id = idList[i]
                 if (await this.hasAttribute(id, attribute) === true) {
                     matchingIds.push(id)
