04a8cfb2cbc51fd60db265521357caf8090bc88b
jjvalkema <jjvalkema@protonmail.com>
2023-08-19 18:51:35 +0200

    made AND and NOT processing functions

--- ui/scripts/uriHandler.js
+++ ui/scripts/uriHandler.js
@@ -9,7 +9,7 @@ class uriHandler {
     baseURICache = null;
     ipfsGateway = null;
     //TODO fix naming
-    constructor(contractObj, extraUriMetaDataFile="./claim/scripts/extraUriMetaDataFile.json", _ipfsGateway="https://ipfs.io", _extraUriMetaData = undefined) {
+    constructor(contractObj, extraUriMetaDataFile = "./claim/scripts/extraUriMetaDataFile.json", _ipfsGateway = "https://ipfs.io", _extraUriMetaData = undefined) {
         this.contractObj = contractObj;
         this.ipfsGateway = _ipfsGateway
         if (extraUriMetaDataFile) {
@@ -58,9 +58,9 @@ class uriHandler {
             } catch {
                 let s = (await nftContract.tokenURI(1))
                 if (s.endsWith("1")) {
-                    this.baseURICache = s.slice(0,-1);
-                } else if(s.endsWith("1.json")) {
-                    this.baseURICache = s.slice(0,-6)
+                    this.baseURICache = s.slice(0, -1);
+                } else if (s.endsWith("1.json")) {
+                    this.baseURICache = s.slice(0, -6)
                 } else {
                     throw error(`NFT contract does not have baseURI() function and it's tokenURI() function return a non standard string: \n ${s}`)
                 }
@@ -105,7 +105,7 @@ class uriHandler {
     }
 
 
-    async syncUriCacheByScraping(startId = 0, endId = null, chunkSize = 100, idList=null) {
+    async syncUriCacheByScraping(startId = 0, endId = null, chunkSize = 100, idList = null) {
         let allUris = [];
         //iter from startId to endId
         if (idList === null) {
@@ -152,7 +152,7 @@ class uriHandler {
         }
     }
 
-    async syncUriCache(startId=0, endId=null, chunkSize=100) {
+    async syncUriCache(startId = 0, endId = null, chunkSize = 100) {
         console.log((await this.extraUriMetaData).scrapedUriData)
         if ((await this.extraUriMetaData).scrapedUriData) {
             this.uriCache = await (await this.getUrlByProtocol((await this.extraUriMetaData).scrapedUriData)).json()
@@ -160,7 +160,7 @@ class uriHandler {
             console.log(`no premade metadata found for ntf contract: ${await this.contractObj.address} :( collecting attribute manually!`)
             //syncUriCacheByScraping already
             //this.uriCache = 
-            this.uriCache =  await this.syncUriCacheByScraping(startId, endId, chunkSize)
+            this.uriCache = await this.syncUriCacheByScraping(startId, endId, chunkSize)
         }
     }
 
@@ -234,23 +234,24 @@ class uriHandler {
      * @param {Int} startId 
      * @param {Int} endId 
      * @param {Set} idSet 
+     * @param {Set} excludeIdSet
      * @return {Set} matchingIds
      */
-    async getIdsByAttribute(attribute, startId = 0, endId = null, idSet = null) {
+    async getIdsByAttribute(attribute, startId = 0, endId = null, idSet = null, excludeIdSet = new Set()) {
         let matchingIds = new Set()
-        if (idSet === null) {
+        if (!idSet || idSet.size === 0) {
             if (endId === null) {
                 endId = (await this.contractObj.totalSupply()).toNumber()
             }
             for (let id = startId; id < endId; id++) {
                 //console.log(id)
-                if (await this.hasAttribute(id, attribute) === true) {
+                if (!excludeIdSet.has(id) && await this.hasAttribute(id, attribute) === true) {
                     matchingIds.add(id)
                 }
             }
         } else {
             for (const id of idSet.size) {
-                if (await this.hasAttribute(id, attribute) === true) {
+                if (!excludeIdSet.has(id) && await this.hasAttribute(id, attribute) === true) {
                     matchingIds.add(id)
                 }
             }
@@ -262,7 +263,7 @@ class uriHandler {
      * 
      * @param {Set} idSet 
      */
-    async removeAttributeFromSet(attribute,idSet) {
+    async removeAttributeFromSet(attribute, idSet) {
         for (const id of idSet) {
             if (await this.hasAttribute(id, attribute) === true) {
                 idSet.delete(id)
@@ -287,11 +288,121 @@ class uriHandler {
                     idSet.add(id)
                 }
             }
-        
+
         }
         return idSet
 
         //range(startId,endId)
         //let newIdSet = new Set([...Array(endId-startId).keys()].map(i => i + startId));
+    }
+    intersection(setA, setB) {
+        let intersectionSet = new Set();
+    
+        for (let i of setB) {
+            if (setA.has(i)) {
+                intersectionSet.add(i);
+            }
+        }
+        return intersectionSet;
+    }
+
+
+    /**
+     * 
+     * @param {Object} inputs 
+     * @return {Object} idSet
+     */
+    async processAndCondition(inputs) {
+        const inputTypesOrder = ["idList", "attributes", "conditions"]
+        const excludeIdSet = new Set(inputs.NOT)
+        let idSet = new Set();
+        for (const type in inputTypesOrder) {
+            switch (type) {
+                //TODO check order
+                case ("idList"): 
+                    idSet = new Set(inputs.idList) //idList = ids that we be checked if they have specified attributes and conditions
+                    break
+                case ("attributes"):
+                    for (attribute in inputs.attributes) {
+                        let attrIds = await this.getIdsByAttribute(attribute, 0, null, idSet, excludeIdSet)
+                        idSet = new Set(attrIds)
+                    }
+                    break
+
+                case ("conditions"):
+                    for (condition in inputs.conditions) {
+                        const newSet = await this.processCondition(condition)
+                        idSet = this.intersection(idSet, newSet);
+                    }
+                    break
+            }
+
+        }
+        return idSet
+    }
+
+
+     /**
+     * 
+     * @param {Object} inputs 
+     * @return {Object} idSet
+     */
+    async processOrCondition(inputs) {
+        const inputTypesOrder = ["idList", "attributes", "conditions"]
+        let idSet = new Set();
+        for (const type in inputTypesOrder) {
+            switch (type) {
+                //TODO check order
+                case ("idList"): 
+                    idSet = new Set(inputs.idList) //idList = ids that we add to everything else
+                    break
+                case ("attributes"):
+                    for (attribute in inputs.attributes) {
+                        let newIdSet = await this.getIdsByAttribute(attribute, 0, null, null, idSet) //idSet excluded so theyre not processed (they're allready)
+                        idSet = new Set([...idSet, ...newIdSet])
+                    }
+                    break
+
+                case ("conditions"):
+                    for (condition in inputs.conditions) {
+                        const newSet = await this.processCondition(condition)
+                        idSet = new Set([...idSet, ...newIdSet])
+                    }
+                    break
+            }
+
+        }
+        return idSet
+
+    }
+
+    /**
+     * 
+     * @param {Object} condition 
+     * @returns {Set} idSet
+     */
+    async processCondition(condition) {
+        //{"type":"OR", "input":{"idList":[]],"conditions":[], "attributes":[]}, "NOT":{"idList":[]],"conditions":[],"attributes":[]]}}
+        switch (condition.type){
+            case ("AND"):
+                break
+            case ("OR"):
+                break
+            case ("RANGE"):
+                break
+            case ("NOT"):
+                //prob not needed
+                break
+
+        }
+
+    }
+
+
+    async attributeFilter(filter) {
+
+
+
+        //TODO make and or not
     }
 }
