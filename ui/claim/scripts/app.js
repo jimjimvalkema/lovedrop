d9efe0cb9ec645986848b5e0b769e9af11c9fd90
jjvalkema <jjvalkema@protonmail.com>
2023-10-08 19:47:25 +0200

    started redoin the claim page

--- ui/claim/scripts/app.js
+++ ui/claim/scripts/app.js
@@ -1,83 +1,45 @@
-// maybe switch to window :p
-let mildayDropAddress = null;
-let mildayDropContract = null;
-let mildayDropWithSigner = null;
-
-let airdropTokenAddress = null;
-let airdropTokenContract = null;
-
-let nftContractAddress = null;
-let nftContract = null;
-
-let signer = null;
-
-let currentFileString = null;
-let currenFileName = null;
-
-//abis global variable
-let mildayDropAbi = null;
-let ERC20ABI = null;
-let ERC721ABI = null;
-
-window.usersChosenIdsToClaim =[];
+import { uriHandler } from "../../scripts/uriHandler.js";
+import { IpfsIndexer } from "../../scripts/IpfsIndexer.js";
+import { ethers } from "../../scripts/ethers-5.2.esm.min.js";
+window.uriHandler = uriHandler
 
 // A Web3Provider wraps a standard Web3 provider, which is
 // what MetaMask injects as window.ethereum into each page
 // TODO get 3rd party provider incase user doesnt connect 
 const provider = new ethers.providers.Web3Provider(window.ethereum)
 
+if (window.ethereum) {
+    window.provider = new ethers.providers.Web3Provider(window.ethereum);
+} else {
+    console.log("couldn't connect to inject ethereum provide, connecting to external provicer")
+    window.provider = new ethers.providers.JsonRpcProvider('https://eth.llamarpc.com');
+}
+
+function isWalletConnected() {
+    let message = "";
+    if (window.signer == null) {
+        message = "wallet not connected";
+        console.log(message);
+        document.getElementById("message2").innerHTML = message;
+        return false
+    } else {
+        document.getElementById("message2").innerHTML = message;
+        return true;
+    }
+}
+
 
 async function connectSigner() {
     // MetaMask requires requesting permission to connect users accounts
     provider.send("eth_requestAccounts", []);
-    signer = provider.getSigner();
+    window.signer = provider.getSigner();
     if (isWalletConnected()) {
         await loadAllContracts();
         return [provider, signer];
     }
 }
+window.connectSigner = connectSigner
 
-async function getMiladyDropContract(provider, urlVars) {
-    mildayDropAddress = urlVars["mildayDropAddress"]
-    //const mildayDropAbi = 
-    // The Contract object
-    mildayDropContract = new ethers.Contract(mildayDropAddress, mildayDropAbi, provider);
-    return mildayDropContract;
-}
-
-async function getAirdropTokenContract() {
-    const airDropTokenAddress = await mildayDropContract.airdropTokenAddress();
-    // The ERC-20 Contract ABI, which is a common contract interface
-    // for tokens (this is the Human-Readable ABI format)
-    // The Contract object
-    const airdropTokenContract = new ethers.Contract(airDropTokenAddress, ERC20ABI, provider);
-    return airdropTokenContract;
-}
-
-async function getNftContract() {
-    nftContractAddress = await mildayDropContract.requiredNFTAddress();
-    // The Contract object
-    nftContract = new ethers.Contract(nftContractAddress, ERC721ABI, provider);
-    return nftContract;
-
-}
-
-async function getUserNftIds(userAddress) {
-    var ids = [];
-    let userBalance = (await nftContract.balanceOf(userAddress)).toNumber();
-    for (let i = 0; i < userBalance; i++) {
-        ids.push((await nftContract.tokenOfOwnerByIndex(userAddress, i)).toNumber());
-
-    }
-    return ids;
-}
-
-function isClaimed(claimData) {
-    if (claimData===null) {
-        return true
-    }
-    return mildayDropWithSigner.isClaimed(claimData["index"]);
-}
 
 async function getUrlVars() {
     var vars = {};
@@ -87,137 +49,47 @@ async function getUrlVars() {
     return vars;
 }
 
-function isWalletConnected() {
-    let message = "";
-    if (signer == null) {
-        message = "wallet not connected";
-        console.log(message);
-        document.getElementById("message").innerHTML = message;
-        return false
-    } else {
-        document.getElementById("message").innerHTML = message;
-        return true;
-    }
-}
-
-function getProof(id, claimData) {
-    const index = claimData["index"];
-    const amount = parseInt(claimData["amount"], 16)
-    const proof = claimData["proof"];
-    return [index, id, amount, proof]
-
-}
 
 
-async function claimAll(ipfsIndex=window.ipfsIndex) {
-    if (isWalletConnected()) {
-        //await getAllContracts();
-        let userAddress = await signer.getAddress();
-        var user_nfts = await getUserNftIds(userAddress, nftContract);
-        var unclaimed_proofs = [];
-        for (let i = 0; i < user_nfts.length; i++) {
-            let id = user_nfts[i]
-            let claimData = await ipfsIndex.getIdFromIndex(id) //TODO handle error if doesn't exist and message to user
-            if (claimData != null) {
-                if (! await isClaimed(claimData)) {
-                    unclaimed_proofs.push(getProof(id, claimData));
-                    console.log(`id: ${id} is added to claim all!`)
-                } else {
-                    //TODO give users this info
-                    console.log(`id: ${id} is in wallet but already claimed`)
-                }
-            } else {
-                console.log(`ID: ${id} is in wallet but not in the claims index`)
-            }
-        }
-        if (unclaimed_proofs.length == 0) {
-            let nftBalance =  (await nftContract.balanceOf(userAddress)).toNumber();
-            let message = `found ${nftBalance} nfts but none have tokens to be claimed`;
-            document.getElementById("message").innerHTML = message;
-            return 0
-        }
-        mildayDropWithSigner.claimMultiple(unclaimed_proofs); //TODO unclaimed_proofs to the struct
-    }
-    return 0
-};
-
-async function claimList(ids=window.usersChosenIdsToClaim) {
-    if (isWalletConnected()) {
-        let userAddress = await signer.getAddress();
-        var unclaimed_proofs = [];
-        for (let i = 0; i < ids.length; i++) {
-            let id = ids[i]
-            let claimData = await ipfsIndex.getIdFromIndex(id) //TODO handle error if doesn't exist and message to user
-            if (claimData != null) {
-                if (! await isClaimed(claimData)) {
-                    unclaimed_proofs.push(getProof(id, claimData));
-                    console.log(`id: ${id} is added to claim all!`)
-                } else {
-                    //TODO give users this info
-                    console.log(`id: ${id} is in wallet but already claimed`)
-                }
-            } else {
-                console.log(`ID: ${id} is in wallet but not in the claims index`)
-            }
-        }
-        if (unclaimed_proofs.length == 0) {
-            let nftBalance =  (await nftContract.balanceOf(userAddress)).toNumber();
-            let message = `found ${nftBalance} nfts but none have tokens to be claimed`;
-            document.getElementById("message").innerHTML = message;
-            return 0
-        }
-        mildayDropWithSigner.claimMultiple(unclaimed_proofs); //TODO unclaimed_proofs to the struct
-        return 0
-    }
-}
 
 
-async function claim(id, ipfsIndex=window.ipfsIndex) {
-    const claimData = await ipfsIndex.getIdFromIndex(id)
-    if (isWalletConnected()) {
-        if (! await isClaimed(claimData)) {
-            let proof = await getProof(id, claimData);
-            mildayDropWithSigner.claim(...proof);
-        }
-    }
-    return 0;
-};
-
-function isInterger(string) {
-    
-    if(isNaN(parseInt(string))){
-        return(false)
-    } else {
-        return(true);
-    }
-};
-
-async function displayNFTS(URI, ids) {
-    let imagesHTML = ""
+async function displayNFTSForClaims(URI, ids) {
+    let unClaimedImagesHTML = "";
+    let claimedImagesHTML = "";
+    let noClaimImagesHTML = ""
+    const ticker = await airdropTokenContract.symbol()
     for (let i = 0; i < ids.length; i++ ) {
         console.log(ids[i])
         url = await URI.getImage(ids[i])
         let claimData = await ipfsIndex.getIdFromIndex(ids[i])
         if (claimData===null) {
-            imagesHTML += `<div id="NFT${ids[i]}" style="border:5px solid black; width: 20%; display: inline-block;" >
-            <h3>Nothing to claim :( </h3>
-            <img src="${url}" style="max-width: 100%; max-height: 100%;">\n 
+            noClaimImagesHTML += `<div id="NFT${ids[i]}" style="position: relative; margin: 2px; border:5px solid black; width: 20%; display: inline-block;" >
+                <img src="${url}" style="max-width: 100%; max-height: 100%;">\n 
+                <div  style="float: right; position: absolute; left: 0px; bottom: 0px; z-index: 1; background-color: rgba(0, 0, 0, 0.8); padding: 5px; color: #FFFFFF; font-weight: bold;">
+                    Not eligable for ${ticker} :(
+                </div>
             </div>`;
             continue
         }
         if ( await isClaimed(claimData)) {
-            imagesHTML += `<div id="NFT${ids[i]}" style="border:5px solid black; width: 20%; display: inline-block;" >
-                <h3>Already claimed :( </h3>
-                <img src="${url}" style="max-width: 100%; max-height: 100%;">\n 
+            amount = parseInt(claimData["amount"], 16);
+            claimedImagesHTML += `<div id="NFT${ids[i]}" style="position: relative; margin: 2px; border:5px solid black; width: 20%; display: inline-block;" >
+                    <img src="${url}" style="max-width: 100%; max-height: 100%;">\n 
+                    <div  style="float: right; position: absolute; left: 0px; bottom: 0px; z-index: 1; background-color:  rgba(0, 0, 0, 0.8); padding: 5px; color: #FFFFFF; font-weight: bold;">
+                        ${amount} ${ticker} already claimed :/
+                    </div>
                 </div>`;
         } else {
-            imagesHTML += `<div id="NFT${ids[i]}" onclick="toggleUsersChosenIdsToClaim(${ids[i]})" style="cursor:pointer; border:5px solid green; width: 20%; display: inline-block;" >
-                <h3>Claim me!!!</h3>
-                <img src="${url}" style="max-width: 100%; max-height: 100%;">\n 
+            amount = parseInt(claimData["amount"], 16);
+            unClaimedImagesHTML += `<div id="NFT${ids[i]}" onclick="toggleUsersChosenIdsToClaim(${ids[i]})" style="position: relative; margin: 2px; cursor:pointer; border:5px solid green; width: 20%; display: inline-block;" >
+                    <img src="${url}" style="max-width: 100%; max-height: 100%;">\n 
+                    <div  style="float: right; position: absolute; left: 0px; bottom: 0px; z-index: 1; background-color: rgba(20, 200, 20, 0.8); padding: 5px; color: #FFFFFF; font-weight: bold;">
+                        ${amount} ${ticker} claimable :D
+                    </div>
                 </div>`;
         }
     };
-    document.getElementById("nftImages").innerHTML = imagesHTML;
+    document.getElementById("nftImages").innerHTML = unClaimedImagesHTML + claimedImagesHTML + noClaimImagesHTML;
 } 
 
 function toggleUsersChosenIdsToClaim(id) {
@@ -235,65 +107,47 @@ function toggleUsersChosenIdsToClaim(id) {
         document.getElementById(`NFT${id}`).style.border = "5px solid blue";
     }
 }
-    
-
-function goToclaim() {
-    window.x = document.getElementById("infuraIpfsForm").elements;
-    const currentUrl = new URL(window.location.href)
-    //TODO set address
-    location.replace(`/?mildayDropAddress=0x326a3c40DfD5D46bbFbcaa3530C1E71120fc603a&ipfsApi=${x.ipfsApi.value}&projectId=${x.projectId.value}&projectSecret=${x.keySecret.value}`)
-  }
-
-async function test() {
-    if (!isWalletConnected()) {
-        return 0
-    }
-
-    console.log(nftContract);
-    URI = new uriHandler(nftContract, "./scripts/URITypes.json");
-    console.log(await URI.getImage(1));
-    let userNfts = await getUserNftIds(await signer.getAddress());
-    console.log(userNfts);
-    displayNFTS(URI ,userNfts);
-
-};
-
-window.onload = runOnLoad;
 
 async function runOnLoad() {
-    window.urlVars = await getUrlVars();
-    let mildayDropAbiFile = await fetch('./../abi/mildayDropAbi.json');
-    let ERC721ABIFile = await fetch('./../abi/ERC721ABI.json');
-    let ERC20ABIFile = await fetch('./../abi/ERC20ABI.json');
-    mildayDropAbi = await mildayDropAbiFile.json();
-    ERC721ABI = await ERC721ABIFile.json();
-    ERC20ABI = await ERC20ABIFile.json();
+    await loadAllContracts()
 
-    //TODO make function to connect ipfs indexer and option for is gateway in ui
-    //TODO test if can connect 
-    //connect api
-    window.ipfsApi= window.urlVars["ipfsApi"]   
-    window.auth = null; 
-    if(window.urlVars["projectId"]!=null) {
-        const projectId = window.urlVars["projectId"]
-        const projectSecret = window.urlVars["projectSecret"]
-        window.auth  = "Basic " + btoa(projectId+ ":" + projectSecret);
-    
-    } else {
-        window.auth  = null
-    }
-    window.ipfsIndex = new ipfsIndexer(window.ipfsApi, window.auth , isGateway=true);
 }
+window.onload = runOnLoad;
 
 async function loadAllContracts() {
-    urlVars = await getUrlVars();
-    mildayDropContract = await getMiladyDropContract(provider, urlVars);
-    mildayDropWithSigner = mildayDropContract.connect(signer);
-    nftContract = await getNftContract();
-    airdropTokenContract = await getAirdropTokenContract();
-    //load indexer
-    claimDataIpfsHash = "bafybeih34n353h3qvgq5rtpgawnmqaqg3nlxnowtt3hdqizlaffhj4efeq"//await mildayDropContract.claimDataIpfs(); //await window.ipfsIndex.createIpfsIndex(balanceMapJson, splitSize=780);//"bafybeigdvozivwvzn3mrckxeptstjxzvtpgmzqsxbau5qecovlh4r57tci"
-    await window.ipfsIndex.loadIndex(claimDataIpfsHash);
+    window.urlVars = await getUrlVars();
+    window.ipfsGateway = window.urlVars["ipfsGateway"]
 
-    return [mildayDropContract, mildayDropWithSigner, nftContract, airdropTokenContract];
+    //abis
+    const mildayDropAbi = await (await fetch("../abi/mildayDropAbi.json")).json()//update mildayDropAbi.json
+    const ERC721ABI = await (await fetch("../abi/ERC721ABI.json")).json()
+
+    //miladyDrop Contract
+    window.mildayDropContract = new ethers.Contract(window.urlVars["mildayDropAddress"], mildayDropAbi, window.provider);
+    
+    //do in connect wallet
+    //window.mildayDropWithSigner = window.mildayDropContract.connect(signer);
+
+    
+    //load ipfs data
+    const claimDataIpfsHash = await mildayDropContract.claimDataIpfs(); //await window.ipfsIndex.createIpfsIndex(balanceMapJson, splitSize=780);//"bafybeigdvozivwvzn3mrckxeptstjxzvtpgmzqsxbau5qecovlh4r57tci"
+    window.ipfsIndex = new IpfsIndexer(window.urlVars["ipfsGateway"],null,true)
+    await window.ipfsIndex.loadIndexMiladyDropClaimData(claimDataIpfsHash)
+
+    //get all nft contracts
+    window.allNftAddresses = await window.ipfsIndex.getAllNftAddrs()
+    window.allNftContractObj = allNftAddresses.map((address)=> new ethers.Contract(address, ERC721ABI, window.provider))
+    Promise.all(allNftContractObj)
+    window.allUriHandlers = allNftContractObj.map((contractObj)=>new uriHandler(contractObj,window.ipfsGateway,true, "../../scripts/extraUriMetaDataFile.json",window.provider ))
+
+
 }
+window.loadAllContracts = loadAllContracts
+
+async function test() {
+    const ids = await window.allUriHandlers[0].getIdsOfowner(await window.signer.getAddress())
+    console.log(ids)
+    console.log(await ipfsIndex.getProof("0xbf4a480D3009348FDF523A43Ba33220566fC1e4E",ids[0]))
+
+}
+window.test = test
