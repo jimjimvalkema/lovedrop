ae661e26dfdd33ddb395bc82cd3e58fa89b02665
jjvalkema <jjvalkema@protonmail.com>
2024-04-03 23:52:48 +0200

    cleaned up the collected indexed meta data

--- ui/scripts/extraMetaData.js
+++ ui/scripts/extraMetaData.js
@@ -1,81 +1,86 @@
 export const allExtraMetaData ={
-    "0xbf4a480D3009348FDF523A43Ba33220566fC1e4E": {
-        "type": "milady",
-        "name": "miladyTest",
-        "isTest":true,
-        "idStartsAt":0,
-        "totalSupply":9,
-        "correctedMetaDataSeparatedFiles":"bafybeiawqw7zaoliz2rjgiqwzyykwzjsmr24i3a6paazalmqijsldtfg7i",
-        "compressedImages": "ipfs://bafybeidku5upsi2oooyummqkgacv43ab6dqcnkji7pfq73oqx4l6zl362a",
-        "metaDataArray" : "ipfs://bafybeibexus34rk6bt7psn7gly2sduq3xui4jzshoimmdvrw3otqzglp3y",
-        "metaDataArrayCbor": "ipfs://bafybeic7f5slx2tqmkt3uj6573cvdtfgwk3pg3xyuu2jra7klyohkh2sii",
-        "idsPerAttribute": "ipfs://bafybeicgayygxqzdsz37hkn3shfpsxrbyyiemodpyrpkoazxbb3p5khc5m",
-        "idsPerAttributeCbor":"ipfs://bafybeigey4mq6pxl7zk77rwls7glb46pjzxhm5rf33eu6fbzfh4pwoktmy" 
-    },
-    "0x08e89a2a1660CE48f3178b4Ea88F49b7593576ba": {
-        "type": "standard",
-        "name": "mifairymaker",
-        "isTest":true,
-        "idStartsAt":1,
-        "totalSupply":9,
-        "compressedImages": "ipfs://bafybeigydxenstqykovw3hpfsqyf46zkvezkigs52shqno4rul4cpznl7i",
-        "metaDataArray" : "ipfs://bafybeigweabg4xfyrzv3envrqfrfyj5hsr6abw5ijmvxbygiozxeavt6me",
-        "metaDataArrayCbor": "ipfs://bafybeih62dlui4td2bcwxlgjkn63lz2ibqp7i2k3s6a3zmm4xxioj4m6hq",
-        "idsPerAttribute": "ipfs://bafybeiek26panjzlaqif5ovolak7bgz4kjj7lbsfupixnytjxj47lcu5dq",
-        "idsPerAttributeCbor":"ipfs://bafkreidwkpbcw2gzxufhnjyamfkl3izdkfzwugzlerzrodr5xldr6ikwwq",
-        "imageFileExtesion":".png"
-    },
     "0x5Af0D9827E0c53E4799BB226655A1de152A425a5" : {
         "type": "milady",
-        "name": "miladyMainnet",
-        "correctedMetaDataSeparatedFiles":"bafybeiawqw7zaoliz2rjgiqwzyykwzjsmr24i3a6paazalmqijsldtfg7i",
-        "compressedImages": "ipfs://bafybeidku5upsi2oooyummqkgacv43ab6dqcnkji7pfq73oqx4l6zl362a",
-        "metaDataArray" : "ipfs://bafybeibexus34rk6bt7psn7gly2sduq3xui4jzshoimmdvrw3otqzglp3y",
-        "metaDataArrayCbor": "ipfs://bafybeic7f5slx2tqmkt3uj6573cvdtfgwk3pg3xyuu2jra7klyohkh2sii",
-        "idsPerAttribute": "ipfs://bafybeicgayygxqzdsz37hkn3shfpsxrbyyiemodpyrpkoazxbb3p5khc5m",
-        "idsPerAttributeCbor":"ipfs://bafybeigey4mq6pxl7zk77rwls7glb46pjzxhm5rf33eu6fbzfh4pwoktmy"
+        "name": "milady",
 
+        "idStartsAt":0,
+        "idsEndsAt":9999,
+
+        "idsPerAttribute": "ipfs://bafybeicgayygxqzdsz37hkn3shfpsxrbyyiemodpyrpkoazxbb3p5khc5m",
+        "compressedImages": "ipfs://bafybeidku5upsi2oooyummqkgacv43ab6dqcnkji7pfq73oqx4l6zl362a",
+        "imageFileExtesion":".jpg",
+
+        "baseUri": "ipfs://bafybeibkjh7jetwch4vzlyb2yq7tdqldqghnrzcqz44yswtshqqch73afm",
+        
+        //unused
+        "metaDataArray" : "ipfs://bafybeibexus34rk6bt7psn7gly2sduq3xui4jzshoimmdvrw3otqzglp3y",
+        
+    },
+
+    "0x1D20A51F088492A0f1C57f047A9e30c9aB5C07Ea": {
+        "name": "loomlocknft",
+        "type": "standard",
+        "idStartsAt": 0,
+        "idEndsAt": 12344,
+        "totalsupply": 12343,
+        "idsPerAttribute": "ipfs://bafybeiawcgynr55syd4klqykscatiqtt3tsirjj4eb5xfhjwfnpzpw3ctu",
+        "baseUri": "https://arweave.net/LGlMDKAWgcDyvYoft1YV6Y2pBBAwjWaFuZrDP9yD-RY",
+        "baseUriExtension": ".json"
+      },
+
+    "0x67B5eE6e29a4230177Dda07AD7848e42d89cF9a0": {
+        "type": "standard",
+        "name": "mifairymaker",
+
+        "idStartsAt":1,
+        "totalSupply":3333,
+
+        "idsPerAttribute": "ipfs://bafybeiek26panjzlaqif5ovolak7bgz4kjj7lbsfupixnytjxj47lcu5dq",
+        "compressedImages": "ipfs://bafybeigydxenstqykovw3hpfsqyf46zkvezkigs52shqno4rul4cpznl7i",
+        "imageFileExtesion":".png",
+       
+        //unused
+        "metaDataArray" : "ipfs://bafybeigweabg4xfyrzv3envrqfrfyj5hsr6abw5ijmvxbygiozxeavt6me",
     },
 
     "0xe2461caf2FfEf37bFC5a41555600Dbe65737e1bD" : {
         "type": "blockedCors",
         "name": "CHWO",
-        "correctedMetaDataSeparatedFiles":"QmNvyYsgbqdBDQTXMdLk6h4LuvUCgkSKgJFLyUVSYvGV1j",
-        "metaDataArray" : "ipfs://bafybeiabyaop3lmrv5c7amwijfr74uqbiwegiars3kcvqkwjbe6mpjjufa",
+
+        // "idStartsAt":undefined,
+        // "idsEndsAt":undefined,
+
         "idsPerAttribute": "ipfs://bafkreifhraqwqwrdxc6qsz2dzhr53bfodon27fsvqia62sr3jpsofshqyq",
-        "idsPerAttributeCbor":"ipfs://bafkreiaruwgujisqg5527vdpegplimycp5da7b5ra52jtk4nmmq6ekpxmi",
-        "compressedImages":"https://reveal.cakehats.xyz",
-        "imageFileExtesion":".png"
+        "compressedImages":"https://reveal.cakehats.xyz", //not compressed :p
+        "imageFileExtesion":".png",
+
+        "baseUri":"ipfs://QmNvyYsgbqdBDQTXMdLk6h4LuvUCgkSKgJFLyUVSYvGV1j",
         
+        //unused
+        "metaDataArray" : "ipfs://bafybeiabyaop3lmrv5c7amwijfr74uqbiwegiars3kcvqkwjbe6mpjjufa",
     },
 
-    "0x67B5eE6e29a4230177Dda07AD7848e42d89cF9a0" : {
-        "type": "standard",
-        "name": "mifairymaker",
-        "idStartsAt":1,
-        "metaDataArray" : "ipfs://bafybeigweabg4xfyrzv3envrqfrfyj5hsr6abw5ijmvxbygiozxeavt6me",
-        "metaDataArrayCbor": "ipfs://bafybeih62dlui4td2bcwxlgjkn63lz2ibqp7i2k3s6a3zmm4xxioj4m6hq",
-        "idsPerAttribute": "ipfs://bafybeiek26panjzlaqif5ovolak7bgz4kjj7lbsfupixnytjxj47lcu5dq",
-        "idsPerAttributeCbor":"ipfs://bafkreidwkpbcw2gzxufhnjyamfkl3izdkfzwugzlerzrodr5xldr6ikwwq"
-    },
 
-    "0xb6a37b5d14d502c3ab0ae6f3a0e058bc9517786e" : {
-        "comment":"alternative images need to be made on ipfs becuase Azukis cors policy is restrictive",
-        "type": "standard",
-        "name": "Azuki Elementals",
-        "idStartsAt":0,
-        "totalSupply":13627,
-        "idsPerAttributeCbor": "ipfs://QmV9xUDfvCcXUd2CD4CnBzWB4DpShhofsnyH1GfvrM724G"
-    },
+    // "0xb6a37b5d14d502c3ab0ae6f3a0e058bc9517786e" : {
+    //     //alternative images need to be made on ipfs becuase Azukis cors policy is restrictive
+    //     //also the links are hashes instead of token ids
+    //     "type": "blockedCors",
+    //     "name": "Azuki Elementals",
 
-    "oldMiladyDataWithout9999": {
-        "metaDataArray" : "ipfs://bafybeigkwunx3qniepqbw6axe3xbylhvnmqwuklgphqfh6i2k47fe37ylu",
-        "metaDataArrayCbor": "ipfs://bafybeiew5nk2a37qfdthij7dtqjilwd37mrtj4bam2svbkuggsfc6oeiiu",
-        "idsPerAttribute": "ipfs://bafybeifjcawdoxgq3z5wzyvdekz67jf5vxxpghdimkq5xcby5y5czloe2q",
-        "idsPerAttributeCbor":"ipfs://bafybeichhvd4y4uyh6z7macd56xh4dk6bzfd5ckzninqbywc56up22kaom",
-        "compressedImages": "ipfs://bafybeiezitb4ewavoxz6tuzn5apatfh7yvq5k4g5rtmebjm757fnpyp4su"
-    }
+    //     "idStartsAt":0,
+    //     "totalSupply":13627,
+        
+    //     // TODO => "idsPerAttribute": "ipfs://",
+    //     "idsPerAttributeCbor": "ipfs://QmV9xUDfvCcXUd2CD4CnBzWB4DpShhofsnyH1GfvrM724G", //lost
+    //     //TOD
+    //     // "compressedImages":""", 
+    //     // "imageFileExtesion":"",
 
+    //     // "baseUri":"ipfs://",
+    // }
+    
+
+    
 
 }
 
