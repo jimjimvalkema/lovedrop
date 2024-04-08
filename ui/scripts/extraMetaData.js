d87581b7f994c44cf414dcdaef090f4a9bf8999e
jjvalkema <jjvalkema@protonmail.com>
2024-04-09 01:04:00 +0200

    added collections

--- ui/scripts/extraMetaData.js
+++ ui/scripts/extraMetaData.js
@@ -1,20 +1,20 @@
-export const allExtraMetaData ={
-    "0x5Af0D9827E0c53E4799BB226655A1de152A425a5" : {
+export const allExtraMetaData = {
+    "0x5Af0D9827E0c53E4799BB226655A1de152A425a5": {
         "type": "milady",
         "name": "milady",
 
-        "firstId":0,
-        "lastId":9999,
+        "firstId": 0,
+        "lastId": 9999,
 
         "idsPerAttribute": "ipfs://bafybeicgayygxqzdsz37hkn3shfpsxrbyyiemodpyrpkoazxbb3p5khc5m",
         "compressedImages": "ipfs://bafybeidku5upsi2oooyummqkgacv43ab6dqcnkji7pfq73oqx4l6zl362a",
-        "imageFileExtesion":".jpg",
+        "imageFileExtesion": ".jpg",
 
         "baseUri": "ipfs://bafybeibkjh7jetwch4vzlyb2yq7tdqldqghnrzcqz44yswtshqqch73afm",
-        
+
         //unused
-        "metaDataArray" : "ipfs://bafybeibexus34rk6bt7psn7gly2sduq3xui4jzshoimmdvrw3otqzglp3y",
-        
+        "metaDataArray": "ipfs://bafybeibexus34rk6bt7psn7gly2sduq3xui4jzshoimmdvrw3otqzglp3y",
+
     },
 
     "0x1D20A51F088492A0f1C57f047A9e30c9aB5C07Ea": {
@@ -26,41 +26,99 @@ export const allExtraMetaData ={
         "idsPerAttribute": "ipfs://bafybeiawcgynr55syd4klqykscatiqtt3tsirjj4eb5xfhjwfnpzpw3ctu",
         "baseUri": "https://arweave.net/LGlMDKAWgcDyvYoft1YV6Y2pBBAwjWaFuZrDP9yD-RY",
         "baseUriExtension": ".json"
-      },
+    },
 
     "0x67B5eE6e29a4230177Dda07AD7848e42d89cF9a0": {
         "type": "standard",
         "name": "mifairymaker",
 
-        "firstId":1,
-        "lastId":3333,
-        "totalSupply":3333,
+        "firstId": 1,
+        "lastId": 3333,
+        "totalSupply": 3333,
 
         "idsPerAttribute": "ipfs://bafybeiek26panjzlaqif5ovolak7bgz4kjj7lbsfupixnytjxj47lcu5dq",
         "baseUri": "ipfs://bafybeiay2g62ppxbtgoklgh2cfoeosdw57roxmo3y67mcjnclntqknqh6e",
         "compressedImages": "ipfs://bafybeigydxenstqykovw3hpfsqyf46zkvezkigs52shqno4rul4cpznl7i",
-        "imageFileExtesion":".png",
-       
+        "imageFileExtesion": ".png",
+
         //unused
-        "metaDataArray" : "ipfs://bafybeigweabg4xfyrzv3envrqfrfyj5hsr6abw5ijmvxbygiozxeavt6me",
+        "metaDataArray": "ipfs://bafybeigweabg4xfyrzv3envrqfrfyj5hsr6abw5ijmvxbygiozxeavt6me",
     },
 
-    "0xe2461caf2FfEf37bFC5a41555600Dbe65737e1bD" : {
+    "0xe2461caf2FfEf37bFC5a41555600Dbe65737e1bD": {
+        "name": "Cake Hat World Order",
         "type": "blockedCors",
-        "name": "CHWO",
-
-        // "firstId":undefined,
-        // "lastId":undefined,
-
+        "firstId": 1,
+        "lastId": 2000,
+        "totalsupply": 2000,
         "idsPerAttribute": "ipfs://bafkreifhraqwqwrdxc6qsz2dzhr53bfodon27fsvqia62sr3jpsofshqyq",
-        "compressedImages":"https://reveal.cakehats.xyz", //not compressed :p
-        "imageFileExtesion":".png",
-
-        "baseUri":"ipfs://QmNvyYsgbqdBDQTXMdLk6h4LuvUCgkSKgJFLyUVSYvGV1j",
-        
-        //unused
-        "metaDataArray" : "ipfs://bafybeiabyaop3lmrv5c7amwijfr74uqbiwegiars3kcvqkwjbe6mpjjufa",
+        "compressedImages": "https://reveal.cakehats.xyz", //not compressed :p
+        "baseUri": "ipfs://QmNvyYsgbqdBDQTXMdLk6h4LuvUCgkSKgJFLyUVSYvGV1j",
+        "metaDataArray": "ipfs://bafybeiabyaop3lmrv5c7amwijfr74uqbiwegiars3kcvqkwjbe6mpjjufa",
     },
+
+    "0x3Fc3a022EB15352D3f5E4e6D6f02BBfC57D9C159": {
+        "name": "Molady Maker",
+        "type": "standard",
+        "firstId": 1,
+        "lastId": 7777,
+        "totalsupply": 7777,
+        "idsPerAttribute": "ipfs://bafybeidwbgywa2au5w7qreynava42mm6fz4trsxvyyonsl5dsrcl6g3cri",
+        "baseUri": "ipfs://bafybeibqkyfajxfkajomrvmiqfhzzzt2m4zxufzpzntuhmuacslhycckua"
+    },
+
+    "0xD3D9ddd0CF0A5F0BFB8f7fcEAe075DF687eAEBaB": {
+        "name": "Redacted Remilio Babies",
+        "type": "blockedCors",
+        "firstId": 1,
+        "lastId": 10000,
+        "totalsupply": 10000,
+        "idsPerAttribute": "ipfs://bafybeidjpbqdg4ijzs5tzkiv4rbabusw6zfogwpwv5bhkf2smrxieexklq",
+        "baseUri": "ipfs://QmTSzj6MK98vyATARPbvMYUFdEiBQiREnkVGN1t7fsggSt"
+    },
+
+    "0x8Fc0D90f2C45a5e7f94904075c952e0943CFCCfd": {
+        "name": "Pixelady Maker",
+        "type": "standard",
+        "firstId": 1,
+        "lastId": 10000,
+        "totalsupply": 10000,
+        "idsPerAttribute": "ipfs://bafybeih73haeiaiyxd7ogsd6lu2zdssy7tddwjdwuwnyqwuq6uf3wtdgmy",
+        "baseUri": "ipfs://bafybeigd7557iwardhnwg5kbmg2s7tmuxqkstjeoixu7wunooiywbb3jqq"
+    },
+
+    "0x1352149Cd78D686043B504e7e7D96C5946b0C39c": {
+        "name": "This User... Banners",
+        "type": "blockedCORS",
+        "firstId": 1,
+        "lastId": 2000,
+        "totalsupply": 2000,
+        "idsPerAttribute": "ipfs://bafkreibjirflknnfvngxb3gu3tnga3otrdop4gleukbh5okryy7iwn2ndm",
+        "baseUri": "ipfs://QmWqqamRvKRR796Kp5bQKM6BupPu1M1zrToQ7KcBw97iZk"
+    },
+
+    "0x09f66a094a0070EBDdeFA192a33fa5d75b59D46b": {
+        "name": "YAYO NFT",
+        "type": "blockedCORS",
+        "firstId": 1,
+        "lastId": 4000,
+        "totalsupply": 4000,
+        "idsPerAttribute": "ipfs://bafkreico5ajtpivm6rsi4jr4bsuezjc3lwfsbeuome4uvieysymabu4mr4",
+        "baseUri": "ipfs://QmZF8NPUTmSv6uMgXVSmXftpKb92XFqvX7zCHg9y8YW4Wk"
+    },
+
+    "0x8A45Fb65311aC8434AaD5b8a93D1EbA6Ac4e813b": {
+        "name": "Milady, That B.I.T.C.H.",
+        "type": "blockedCORS",
+        "firstId": 1,
+        "lastId": 333,
+        "totalsupply": 333,
+        "idsPerAttribute": "ipfs://bafkreicecrquzqv2vt75wjg4jw4a5odzmfgusz646545ack3tt5o6l7hkq",
+        "baseUri": "ipfs://QmaLTcU6QzFWdmsFcYoFYNaqhg7cKYExtMsetfcD7sppVi"
+      }
+    
+
+
 
 
     // "0xb6a37b5d14d502c3ab0ae6f3a0e058bc9517786e" : {
@@ -71,7 +129,7 @@ export const allExtraMetaData ={
 
     //     "firstId":0,
     //     "totalSupply":13627,
-        
+
     //     // TODO => "idsPerAttribute": "ipfs://",
     //     "idsPerAttributeCbor": "ipfs://QmV9xUDfvCcXUd2CD4CnBzWB4DpShhofsnyH1GfvrM724G", //lost
     //     //TOD
@@ -80,9 +138,9 @@ export const allExtraMetaData ={
 
     //     // "baseUri":"ipfs://",
     // }
-    
 
-    
+
+
 
 }
 
